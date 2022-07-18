fdc78c06a08ea3ee75c71c36b45f1e377261af20
shristi1106 <63191297+shristi1106@users.noreply.github.com>
2022-07-18 13:21:20 +0530

    Add files via upload

---
+++ src/components/Common/Footer/Footer.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.styles.scss'
+
+const Footer = () => (
+  <footer className="text-center">
+    {new Date().getFullYear()} - &copy; Shristi Verma - 
+    Powered by <a href="https://rawg.io/" target="_blank" rel="noopener noreferrer">rawg.io</a>
+  </footer>
+)
+
+export default Footer;
